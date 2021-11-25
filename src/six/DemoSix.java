61133e6c071cda095f95c8035cb1336c0ed57fd3
sonetakohy <sonetakohy@sonetas-mbp>
2021-11-24 20:01:18 -0500

    Sixth commit...

---
+++ src/six/DemoSix.java
@@ -0,0 +1,13 @@
+package six;
+
+public class DemoSix {
+
+	public static void main(String[] args) {
+			System.out.println("Demo six....");
+	}
+
+	public void addNum(int a, int b) {
+			System.out.println(a + b);
+	}
+
+}
